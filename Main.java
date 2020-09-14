ac3b5292d88e224cdb4550f708bdd06c1161c10c
blisn8405 <blisn8405@wrdsb.ca>
2020-09-14 15:35:58 +0000

    finished question 2

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+// create a Scanner for user input
+  Scanner input = new Scanner(System.in);
+
+  // declare a variable for the first number
+
+  // ask the user for a number
+   System.out.println("Please enter a number:");
+   // initialize a number that needs to be divided by another number
+
+   // declare and initialize a variable for integer1
+   int number1 = input.nextInt();
+
+   // declare and initialize a variable for integer1
+   int number2 = input.nextInt();
+   // declare and initialize the quotient from the 2 numbers
+  System.out.println("The answer is " + (number1%number2));
   }
 }
